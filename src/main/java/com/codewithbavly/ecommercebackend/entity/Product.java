1147f08af2eade33bab8ee5489c51aebe2062af5
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2026-07-20 20:56:34 +0300

    Product entites implemented

--- src/main/java/com/codewithbavly/ecommercebackend/entity/Product.java
+++ src/main/java/com/codewithbavly/ecommercebackend/entity/Product.java
@@ -1,4 +1,39 @@
 package com.codewithbavly.ecommercebackend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private Integer stockQuantity;
+
+    @OneToMany(mappedBy = "product")
+    @Builder.Default
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+
 }
